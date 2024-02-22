9d7bb4686b704ad803d1864b543ed3d7121b6c51
Ivan Balakin <nekonekun@gmail.com>
2024-02-22 11:47:04 +0400

    add warning if already authenticated

--- src/hhrubot/tg/router/resume.py
+++ src/hhrubot/tg/router/resume.py
@@ -2,7 +2,8 @@ from typing import Annotated
 
 from aiogram import Router, flags, F
 from aiogram.filters import Command
-from aiogram.types import Message, WebAppInfo, Chat
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, WebAppInfo
 from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardButton
 from dishka.integrations.aiogram import inject, Depends
 
@@ -16,16 +17,20 @@ resume_router = Router()
 @inject
 async def auth_user(
     message: Message,
+    state: FSMContext,
     *,
     build_login_url: Annotated[BuildLoginURL, Depends()],
 ):
+    context_data = await state.get_data()
+    access_token = context_data.get('access_token')
+    if access_token:
+        text = 'You are already authenticated.\nClick button below if you want to sign in as another user.'
+    else:
+        text = 'Click button below to open hh.ru login page'
     builder = InlineKeyboardBuilder()
     login_url = build_login_url(telegram_id=message.from_user.id)
     builder.add(InlineKeyboardButton(text='Sign in', web_app=WebAppInfo(url=login_url)))
-    await message.answer(
-        'Click button below to open hh.ru login page',
-        reply_markup=builder.as_markup()
-    )
+    await message.answer(text, reply_markup=builder.as_markup())
 
 
 @resume_router.message(Command(commands=['resume']), F.chat.type == 'private')
